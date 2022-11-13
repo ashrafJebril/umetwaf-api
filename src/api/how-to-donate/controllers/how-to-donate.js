18efdb8da86ee2d564a3b180d6d6180fab8c340a
ashraf <ashrafjebril9@gmail.com>
2022-11-13 23:03:19 +0300

    fix

---
+++ src/api/how-to-donate/controllers/how-to-donate.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * how-to-donate controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::how-to-donate.how-to-donate');
---
+++ src/api/how-to-donate/routes/how-to-donate.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * how-to-donate router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::how-to-donate.how-to-donate');
