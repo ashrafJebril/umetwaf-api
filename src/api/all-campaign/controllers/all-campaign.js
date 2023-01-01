1888d308271838674787296c3af32a5875c6e4b5
ashraf <ashrafjebril9@gmail.com>
2023-01-01 22:01:57 +1100

    add server

---
+++ src/api/all-campaign/controllers/all-campaign.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * all-campaign controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::all-campaign.all-campaign');
---
+++ src/api/all-campaign/services/all-campaign.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * all-campaign service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::all-campaign.all-campaign');
