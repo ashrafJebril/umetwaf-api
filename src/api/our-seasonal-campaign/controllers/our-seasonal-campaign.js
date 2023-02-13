187e016eddbc9519c3c4630b04ca7312931a62fd
Abdallah Abukhader <33789877+aabukhader@users.noreply.github.com>
2023-02-14 01:48:54 +0300

    our seasonal camps are done (#1)
    * our seasonal camps are done
    
    * fix all points
    
    ---------
    
    Co-authored-by: a-abukhadir <abdallah.abukhadir@calavista.com>

---
+++ src/api/our-seasonal-campaign/controllers/our-seasonal-campaign.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * our-seasonal-campaign controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::our-seasonal-campaign.our-seasonal-campaign');
---
+++ src/api/our-seasonal-campaign/routes/our-seasonal-campaign.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * our-seasonal-campaign router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::our-seasonal-campaign.our-seasonal-campaign');
