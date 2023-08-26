834ea4b6e2820434b9ec9bc13726f9f53950d3b8
aabukhader <abdallhabukhader@gmail.com>
2023-08-26 12:45:45 +0000

    latest to repo

---
+++ src/api/monthly-donation/controllers/monthly-donation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * monthly-donation controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::monthly-donation.monthly-donation');
---
+++ src/api/monthly-donation/routes/monthly-donation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * monthly-donation router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::monthly-donation.monthly-donation');
